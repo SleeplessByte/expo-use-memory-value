79c39c6cbf1b07a667ab77d065d957365ac13d2c
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2020-06-25 23:33:31 +0200

    Better serializable type

--- index.ts
+++ index.ts
@@ -3,7 +3,13 @@ import {
   setItemAsync as setSecureItemAsync,
   deleteItemAsync as deleteSecureItemAsync,
 } from 'expo-secure-store';
-import { useEffect, useState, useCallback } from 'react';
+import {
+  useEffect,
+  useState,
+  useCallback,
+  Dispatch,
+  SetStateAction,
+} from 'react';
 import isEqual from 'react-fast-compare';
 import { AsyncStorage } from 'react-native';
 
@@ -11,22 +17,38 @@ const getItemAsync = AsyncStorage.getItem;
 const setItemAsync = AsyncStorage.setItem;
 const deleteItemAsync = AsyncStorage.removeItem;
 
+export type Serializable =
+  | boolean
+  | number
+  | string
+  | null
+  | SerializableArray
+  | SerializableMap;
+interface SerializableMap {
+  [key: string]: Serializable;
+}
+interface SerializableArray extends Array<Serializable> {}
+
 export type Unsubscribe = () => void;
-export type Listener<T> = (value: Readonly<AnyValue<T>>) => void;
+export type Listener<T extends Serializable> = (
+  value: Readonly<AnyValue<T>>
+) => void;
 
 export type UndeterminedValue = undefined;
 export type NoValue = null;
-export type Update<T> = (value: Readonly<AnyValue<T>> | undefined) => void;
-export type AnyValue<T> = T | UndeterminedValue;
+export type Update<T extends Serializable> = Dispatch<
+  SetStateAction<AnyValue<T> | undefined>
+>;
+export type AnyValue<T extends Serializable> = T | UndeterminedValue;
 
-export interface AnyMemoryValue<T> {
+export interface AnyMemoryValue<T extends Serializable> {
   current: AnyValue<T>;
   subscribe(listener: Listener<T>, emit?: boolean): Unsubscribe;
   unsubscribe(listener: Listener<T>): void;
   emit(value: AnyValue<T>, store?: boolean, newOnly?: boolean): void;
 }
 
-export class MemoryValue<T> implements AnyMemoryValue<T> {
+export class MemoryValue<T extends Serializable> implements AnyMemoryValue<T> {
   private listeners: Listener<T>[];
   private value: T | undefined;
 
@@ -65,7 +87,8 @@ export class MemoryValue<T> implements AnyMemoryValue<T> {
   }
 }
 
-export class SecureStoredMemoryValue<T> implements AnyMemoryValue<T | NoValue> {
+export class SecureStoredMemoryValue<T extends Serializable>
+  implements AnyMemoryValue<T | NoValue> {
   private value: MemoryValue<T | NoValue>;
 
   constructor(
@@ -140,7 +163,8 @@ export class SecureStoredMemoryValue<T> implements AnyMemoryValue<T | NoValue> {
   }
 }
 
-export class StoredMemoryValue<T> implements AnyMemoryValue<T | NoValue> {
+export class StoredMemoryValue<T extends Serializable>
+  implements AnyMemoryValue<T | NoValue> {
   private value: MemoryValue<T | NoValue>;
 
   constructor(
@@ -213,20 +237,24 @@ export class StoredMemoryValue<T> implements AnyMemoryValue<T | NoValue> {
   }
 }
 
-export function useMemoryValue<T>(
+export function useMemoryValue<T extends Serializable>(
   value: AnyMemoryValue<T>
 ): Readonly<AnyValue<T>> {
   return useMutableMemoryValue(value)[0];
 }
 
-export function useMutableMemoryValue<T>(
+export function useMutableMemoryValue<T extends Serializable>(
   value: AnyMemoryValue<T>
 ): [Readonly<AnyValue<T>>, Update<T>] {
   const [state, setState] = useState<AnyValue<T>>(value.current);
 
   const update = useCallback(
-    (nextValue: AnyValue<T>) => {
-      value.emit(nextValue);
+    (nextValue: AnyValue<T> | ((prev: AnyValue<T>) => AnyValue<T>)) => {
+      if (typeof nextValue === 'function') {
+        value.emit(nextValue(value.current));
+      } else {
+        value.emit(nextValue);
+      }
     },
     [value]
   );
