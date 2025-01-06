e6a1b397ae09edbe4505759334386f8ae7769ed0
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2025-01-07 00:18:06 +0100

    Upgrade to Expo 51

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,17 @@
 module.exports = {
   root: true,
-  extends: ['universe/web'],
+  extends: ['universe/native', 'universe/web'],
   rules: {
     'react/jsx-fragments': [1, 'element'],
     '@typescript-eslint/no-redeclare': 0,
+    'prettier/prettier': 0,
   },
+  overrides: [
+    {
+      files: ['index.ts', '*.ts'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  ],
 };
